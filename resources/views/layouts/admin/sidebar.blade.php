0261a109b439c9b5402438650e401d76e7f2d539
alenalizoti <alenalizoti13@gmail.com>
2025-07-19 18:04:26 +0200

    fix seeder

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -1,6 +1,6 @@
         <div class="sidebar col-auto col-md-3 col-xl-2 px-sm-2 px-0  position-fixed h-100">
             <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-black text-decoration-none">
+                <a href="#" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-black text-decoration-none">
                     <span class="fs-5 d-none d-sm-inline">Admin panel</span>
                 </a>
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
